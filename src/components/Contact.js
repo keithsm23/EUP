b8997bf2fc7ea0dd260bdd85a02ef93f79ffb4f7
adithyans23 <adithyan.shaji@impelsys.com>
2023-03-31 20:13:19 +0530

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,165 @@
-import React from "react";
-const data = [
-  {
-      question: "What is Lorem Ipsum?",
-      answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-  {
-      question: "What is Lorem Ipsum?",
-      answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-  {
-      question: "What is Lorem Ipsum?",
-      answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-  {
-      question: "What is Lorem Ipsum?",
-      answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-]
+import React, {useEffect, useState, useMemo} from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
+import '../styles/AboutUs.css';
+import { convertToRaw, EditorState } from "draft-js";
+import draftToHtmlPuri from "draftjs-to-html";
+import '../styles/contact.css';
+
+
+import {
+ CButton,
+ CCard,
+ CCardHeader,
+ CCol,
+ CRow,
+ CTable,
+ CTableBody,
+ CTableDataCell,
+ CTableHead,
+ CTableHeaderCell,
+ CTableRow,
+ CFormSelect,
+} from '@coreui/react'
+
+
+// const data = [
+
+// {
+
+//         question: "What is Lorem Ipsum?",
+
+//         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+//     },
+
+//     {
+
+//         question: "What is Lorem Ipsum?",
+
+//         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+//     },
+
+//     {
+
+//         question: "What is Lorem Ipsum?",
+
+//         answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+//     },
+// {
+// question: "What is Lorem Ipsum?",
+// answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+// },
+// ]
+
 
 const Contact = () => {
-  return (
-     <div>
-      <section className="bg-img">
-         <p className='md'>SAMPLE PAGE</p>
-         <p className='md-1'>Home/Sample Page</p>
-       </section>
-       <br></br>
-       <section >
-        { data.map((news, i) => {
-          return (
-            <div >
-              <p className="container2">{news.question}</p>
-              <p className="container3">{news.answer}</p>
-            </div>
-          );
-        })}
-        </section>
-     </div>
-  )
+const[contacts, setContacts] = useState([]);
+const[input,setInput] = useState({
+
+});
+
+const getAllData = async ()=>{
+const res=await axios.get(`http://api-cms-poc.iplatformsolutions.com/api/page/get?slug=contact`)
+.then((res) => {
+console.log(res.data.data);
+setContacts([res.data.data]);
+})
+.catch((err) =>{
+}); 
 };
 
+useEffect(()=>{
+
+//   let getToken = localStorage.getItem('token');
+//   const config={
+//   headers:{
+//       token: `${getToken}`,
+//       "Content-Type":"application/json",
+//   }
+//   };
+
+getAllData();
+},[]);
+
+//   const myStyle={
+
+//     backgroundImage: "https://i.pinimg.com/originals/f0/da/44/f0da44dc8397004b068f49242b24ee74.jpg",
+
+//     height:'100vh',
+
+//     marginTop:'-70px',
+
+//     fontSize:'50px',
+
+//     backgroundSize: 'cover',
+
+//     backgroundRepeat: 'no-repeat',
+
+// };
+return (
+<div>
+  <section className="bg-img">
+  {/* <div
+  
+            style={{
+  
+              backgroundImage: `url(data:image/png;base64,${
+  
+                contacts && contacts[0].image
+  
+              })`,
+  
+            }}
+  
+          >
+  
+              <p className="md">Contact</p>  <p className="md-1">Home/Contact</p>
+  
+          </div> */}
+  <p className="md">Contact</p>{" "}
+  <p className="md-1">Home/Contact</p>
+  
+  </section>
+  <br></br>
+  <section>
+  {contacts && contacts.map((contact, i) => {
+  let data = JSON.parse(contact.description);
+  const htmlPuri = draftToHtmlPuri(data);
+  // console.log(htmlPuri);
+  console.log('contact', contact);
+  return (
+  <div className="container3">
+  <p>
+  
+{/* <Link style={{textDecoration:"none", fontWeight:"bold"}} to="/SingleNews"> {blog.title} </Link> */}
+  
+<h2>{contact.title}</h2>
+  
+</p>
+<div
+ style={{backgroundImage: `url(data:image/png;base64,${contact && contact.image})`,
+ height: "200px",
+ width: "200px",
+  }} 
+>
+  <p className="md">Contact</p>{" "}
+  
+  <p className="md-1">Home/Contact</p>
+  
+  </div>
+  <td className="container3">
+  
+  <div dangerouslySetInnerHTML={{ __html: htmlPuri }} />
+  {" "}
+  </td>
+  </div>
+  );
+  })}
+  </section>
+  </div>
+);
+};
 export default Contact;
